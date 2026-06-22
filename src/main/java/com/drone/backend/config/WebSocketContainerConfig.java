ddbccf0c36284c1d085c73d5a5ee2eead21e701f
Mupsy <benjibou02@gmail.com>
2026-06-22 10:01:49 +0200

    Fix : Code 1009 still here

--- src/main/java/com/drone/backend/config/WebSocketContainerConfig.java
+++ src/main/java/com/drone/backend/config/WebSocketContainerConfig.java
@@ -1,7 +1,8 @@
 package com.drone.backend.config;
 
-import jakarta.websocket.server.ServerContainer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.apache.tomcat.websocket.server.WsContextListener;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
@@ -9,12 +10,29 @@ import org.springframework.web.socket.server.standard.ServletServerContainerFact
 @Configuration
 public class WebSocketContainerConfig {
 
+    private static final int MAX_SIZE = 10 * 1024 * 1024; // 10 MB
+
     @Bean
     public ServletServerContainerFactoryBean createWebSocketContainer() {
-        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setMaxBinaryMessageBufferSize(52428800);
-        container.setMaxTextMessageBufferSize(52428800);
-        container.setMaxSessionIdleTimeout(3600000L); 
-        return container;
+        ServletServerContainerFactoryBean factory = new ServletServerContainerFactoryBean();
+        factory.setMaxBinaryMessageBufferSize(MAX_SIZE);
+        factory.setMaxTextMessageBufferSize(MAX_SIZE);
+        factory.setMaxSessionIdleTimeout(3600000L);
+        return factory;
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> tomcatCustomizer() {
+        return factory -> factory.addContextCustomizers(context -> {
+            // Attributs JSR-356 lus par Tomcat avant d'ouvrir la session WS
+            context.setAttribute(
+                "org.apache.tomcat.websocket.binaryBufferSize",
+                MAX_SIZE
+            );
+            context.setAttribute(
+                "org.apache.tomcat.websocket.textBufferSize",
+                MAX_SIZE
+            );
+        });
     }
 }
